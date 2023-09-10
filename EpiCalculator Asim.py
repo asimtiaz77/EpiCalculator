4507a8c43654255b65642547a67e8fb2fba3533a
asimtiaz77 <142457766+asimtiaz77@users.noreply.github.com>
2023-09-09 22:43:54 -0400

    Add files via upload

---
+++ EpiCalculator Asim.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+# prevalence
+population = int(input("Population: "))
+existingcases = int(input("Existing cases: "))
+prevalence = existingcases/population
+print(prevalence)
+print(prevalence * 100000)
+
+"Population: 1780000"
+"Existing Cases: 99"
+
+
+# In[10]:
+
+
+# incidence
+newcases = int(input("New cases: "))
+population = int(input("Population: "))
+incidence = newcases / population
+print(incidence)
+print(incidence * 100000)
+
+"New cases= 19"
+"Population= 1780000"
+
+
+# In[13]:
+
+
+# mortality rate
+dead  =int(input("Number of those who died: "))
+segment =int(input("Number of those who had the disease: "))
+mortalityrate = dead / segment 
+print(mortalityrate)
+print(mortalityrate * 100000)
+
+"Number of those who died = 2"
+"Number of those who had the disease = 99"
+
+
+# In[15]:
+
+
+# years of potential life lost
+lifeexpectancy = 77
+ages = [64, 74]
+print(lifeexpectancy - ages[0])
+print(lifeexpectancy - ages[1])
+
+
+# In[14]:
+
+
+lifelost = [13, 3]
+yppl = sum(lifelost) / len(lifelost)
+print(yppl)
+
+
+# In[17]:
+
+
+# prevalence
+population = int(input("Population: "))
+existingcases = int(input("Existing cases: "))
+prevalence = existingcases / population 
+print(prevalence)
+print(prevalence * 100000)
+
+"Population = 2660000"
+"Existing cases = 1459"
+
+
+# In[31]:
+
+
+# incidence
+newcases - int(input("New cases: "))
+population - int(input("Population: "))
+incidence - newcases / population
+print(incidence)
+print(incidence * 100000)
+
+"New cases: 248"
+"Population: 2660000"
+
+
+# In[20]:
+
+
+# mortality rate
+dead - int(input("Number of those who died: "))
+segment - int(input("Number of those who had the disease: "))
+mortalityrate - dead / segment 
+print(mortalityrate)
+print(mortalityrate * 100000)
+
+"Number of those who died: 1"
+"Number of those who had the disease: 1459"
+
+
+# In[24]:
+
+
+# prevalence
+population = int(input("Population: "))
+existingcases = int(input("Existing cases: "))
+prevalence = existingcases / population 
+print(prevalence)
+print(prevalence * 100000)
+
+"Population = 100000"
+"Existing cases = 3000"
+
+
+# In[25]:
+
+
+# incidence
+newcases - int(input("New cases: "))
+population - int(input("Population: "))
+incidence - newcases / population
+print(incidence)
+print(incidence * 100000)
+
+"New cases: 2500"
+"Population: 100000"
+
+
+# In[27]:
+
+
+# mortality rate
+dead - int(input("Number of those who died: "))
+segment - int(input("Number of those who had the disease: "))
+mortalityrate - dead / segment 
+print(mortalityrate)
+print(mortalityrate * 100000)
+
+"Number of those who died: 1"
+"Number of those who had the disease: 3000"
+
+
+# In[28]:
+
+
+# prevalence
+population = int(input("Population: "))
+existingcases = int(input("Existing cases: "))
+prevalence = existingcases / population 
+print(prevalence)
+print(prevalence * 100000)
+
+"Population = 1780000"
+"Existing cases = 15000"
+
+
+# In[29]:
+
+
+# incidence
+newcases - int(input("New cases: "))
+population - int(input("Population: "))
+incidence - newcases / population
+print(incidence)
+print(incidence * 100000)
+
+"New cases: 3000"
+"Population: 1780000 "
+
+
+# In[33]:
+
+
+# mortality rate
+dead = int(input("Number of those who died: "))
+segment = int(input("Number of those who had the disease: "))
+mortalityrate - dead / segment 
+print(mortalityrate)
+print(mortalityrate * 100000)
+
+"Number of those who died: 5"
+"Number of those who had the disease: 15000"
+
+
+# In[6]:
+
+
+# years of potential life lost
+lifeexpectancy = 77
+ages = [32, 45, 28, 37, 52]
+print(lifeexpectancy - ages[0])
+print(lifeexpectancy - ages[1])
+print(lifeexpectancy - ages[2])
+print(lifeexpectancy - ages[3])
+print(lifeexpectancy - ages[4])
+
